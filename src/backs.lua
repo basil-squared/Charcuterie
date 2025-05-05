6e3c05b6e7f2077f87f0d16de377325003be9d1d
Kasen Engel <kasen.engel@gmail.com>
2025-05-05 10:01:22 -0500

    ersdfg iopknm

---
+++ src/backs.lua
@@ -0,0 +1,22 @@
+deckatlass = SMODS.Atlas({
+    key = "deck_atlas",
+    px = 71,
+    py = 95,
+    path = "joker_atlas.png"
+})
+
+SMODS.Back {
+    key = "deflation_deck",
+    atlas = deckatlass.key,
+    pos = {x = 0, y = 0},
+    loc_txt =  {
+        name = "Deflation Deck",
+        text = {"Start with {C:attention}$#1#{}.", "You are {C:red}unable{}","to earn any more", "through the course of the run"}
+
+    },
+    config = { extra = {money_start = 100}},
+    loc_vars = function(self, info_queue,card)
+        return {vars = {self.config.extra.money_start}}
+    end
+}
+
