612a9fe471d7b793fa00ae635c2ff514b26c9d9b
Kasen Engel <kasen.engel@gmail.com>
2025-05-11 10:14:43 -0500

    s

---
+++ modules/content/jokers/crkjokers/goldencheese.lua
@@ -0,0 +1,10 @@
+SMODS.Joker {
+    key="golden_cheese",
+    atlas = ASPL.G.jokeratlas.key,
+    pos = {x =12, y = 0  },
+    loc_txt = {
+        name = "Golden Cheese Cookie",
+        text = {"Earn {C:attention}double the money!{}","{S:0.5,C:inactive}'Divine Radiance!'"}
+    },
+
+}
