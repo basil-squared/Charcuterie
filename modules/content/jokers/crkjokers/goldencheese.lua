2260759b1077ea032063fea7b2f7cdd23f17d48d
Kasen Engel <kasenengel@protonmail.com>
2025-05-11 22:14:43 -0500

    changes changes

--- modules/content/jokers/crkjokers/goldencheese.lua
+++ modules/content/jokers/crkjokers/goldencheese.lua
@@ -1,10 +1,13 @@
 SMODS.Joker {
     key="golden_cheese",
     atlas = ASPL.G.jokeratlas.key,
-    pos = {x =12, y = 0  },
+    pos = {x =21, y = 0  },
     loc_txt = {
         name = "Golden Cheese Cookie",
         text = {"Earn {C:attention}double the money!{}","{S:0.5,C:inactive}'Divine Radiance!'"}
     },
+    rarity = 4,
+    cost = 10,
+
 
 }
