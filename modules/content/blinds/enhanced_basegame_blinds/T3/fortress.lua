dd0bd25149cc258c233a2ef14e1874d1ba197b58
Kasen Engel <kasen.engel@gmail.com>
2025-05-13 09:35:57 -0500

    e4

---
+++ modules/content/blinds/enhanced_basegame_blinds/T3/fortress.lua
@@ -0,0 +1,13 @@
+SMODS.Blind {
+    key = "blind_fortress",
+    loc_txt = {
+        name = "The Fortress",
+        text = {"Absurdly Large Blind"},
+    },
+    atlas = ASPL.G.blindatlas.key,
+    pos = {x = 0,y =9},
+    boss_colour = HEX("8b14db"),
+	boss = { min = 2, max = 10 },
+    mult = 50,
+
+}
