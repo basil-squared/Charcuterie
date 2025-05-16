2749614d7cf01a7e848beda94ba3b035230a543a
Kasen Engel <kasen.engel@gmail.com>
2025-05-16 10:21:31 -0500

    g

---
+++ modules/content/blinds/equalizer.lua
@@ -0,0 +1,11 @@
+SMODS.Blind {
+    key="equalizer",
+    atlas = ASPL.G.blindatlas.key,
+    pos = {x=0,y=5},
+    mult = 1.5,
+    boss = {min = 4, max = 10000},
+    boss_colour = HEX('f3f3f3'),
+ 
+    
+
+}
