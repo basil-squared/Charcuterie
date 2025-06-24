82c53b3d5e94c6d4d52c31cb43289796da61c44d
Juniper <basil_squared@gmail.com>
2025-06-23 20:19:40 -0500

    (WIP) add some UI in anticipation of T2/T3 Blinds

---
+++ modules/content/blinds/enhanced_basegame_blinds/T2/debtcollector.lua
@@ -0,0 +1,8 @@
+SMODS.Blind {
+    key = "T2_debtcollector",
+    loc_txt = {
+        name = "The Debt Collector",
+        loc_txt = {"Lose $1 per card played.", "If money is under $0, double blind requirement."}
+    }
+}
+
