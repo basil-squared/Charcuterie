0b724851f40bb1f4e255c7cf79d7b40af93e3ba5
Juniper <pgiemigbvm>
2025-08-17 12:42:34 -0500

    time piece shid

---
+++ modules/content/jokers/#3 Rares/time_piece.lua
@@ -0,0 +1,24 @@
+SMODS.Joker {
+    key = 'time_piece',
+    atlas = CHAR.G.jokeratlas2.key,
+    pos = { x = 0, y = 0 },
+    config = { extra = { obtained_ante = 1, curr_ante = 1 } },
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.obtained_ante,  card.ability.extra.curr_ante}}
+    end,
+    add_to_deck = function(self, card, from_debuff)
+        card.ability.extra.obtained_ante = G.GAME.round_resets.ante
+        card.ability.extra.curr_ante = G.GAME.round_resets.ante
+    end,
+    calculate = function(self, card, context)
+        if context.ante_change then
+            card.ability.extra.curr_ante = G.GAME.round_resets.ante
+        end
+        if context.joker_type_destroyed then
+            if context.other_card == card then
+                ease_ante(-(card.ability.extra.current_ante - card.ability.extra.obtained_ante))
+            end
+        end
+    end
+    
+}
--- modules/content/utils/ease_ante.lua
+++ modules/content/utils/ease_ante.lua
@@ -1,6 +1,7 @@
 
 local ease_ante_ref = ease_ante
 function ease_ante(mod)
+  SMODS.calculate_context({ante_change = true})
   CHAR.FUNC.ease_risk(mod * 10)
   return ease_ante_ref(mod)
 end
