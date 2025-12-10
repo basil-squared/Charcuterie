5b370b119d03542bf107520ec78fd8cc0fada786
Juniper <basil.squared1@gmail.com>
2025-12-10 14:01:23 -0600

    Fix buckshot naming. how was this not reported????

--- modules/content/jokers/#3 Rares/buckshot.lua
+++ modules/content/jokers/#3 Rares/buckshot.lua
@@ -6,22 +6,22 @@ SMODS.Joker {
     cost = 7,
     config = {extra = {odds = 5, repetitions = 2}},
     loc_vars = function(self, info_queue, card)
-        local num,denom = SMODS.get_probability_vars(card,1,card.abiility.extra.odds)
+        local num,denom = SMODS.get_probability_vars(card,1,card.ability.extra.odds)
         return {vars = {num,denom}}
     end,
     calculate = function(self,card,context)
         if SMODS.pseudorandom_probability(card,'wepofwjkpfjkpifj',1,card.ability.extra.odds) then
-            
+
                 if context.repetition and context.cardarea == G.play then
                     return {
                         repetitions = card.ability.extra.repetitions
                     }
                 end
-                
-            
+
+
         end
-            
+
         end
-        
-    
+
+
 }
