176ee83e7db83147c08e9033a2a663b9c946f785
Morwen <basilsquared@proton.me>
2026-07-10 11:15:01 -0500

    add the fuckass globals table for revelation

---
+++ modules/init/globals.lua
@@ -0,0 +1,6 @@
+CHAR.G.REVELATIONTABLE = {
+    ["Tarot"] = "p_arcana_normal",
+    ["Planet"] = "p_celestial_normal",
+    ["Spectral"] = "p_spectral_normal",
+    ["ritual"] = "p_charcuterie_ritual_booster_1"
+}
