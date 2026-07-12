e6aff324d77cfc89661aa88660c83b96264afad7
Morwen <basilsquared@proton.me>
2026-07-12 11:51:32 -0500

    get fading joker to work and then also work on loc vars for some cards

--- modules/content/jokers/#1 Commons/fading.lua
+++ modules/content/jokers/#1 Commons/fading.lua
@@ -5,7 +5,30 @@ SMODS.Joker {
     config = {extra = {basenum = 1, basedenom = 10}},
     loc_vars = function(self,info_queue,card)
         local num,denom = SMODS.get_probability_vars(card,card.ability.extra.basenum,card.ability.extra.basedenom,'charcuterie_fading')
-        info_queue[#info_queue+1] = {set = "Edition",key='e_charcuterie_bleached'}
+        info_queue[#info_queue+1] = {set = "Edition",key='e_charcuterie_bleached',config = {}}
         return { vars = {num,denom}}
+    end,
+    rarity = 1,
+    cost = 4,
+    calculate = function(self,card,context)
+        if context.discard then
+            if SMODS.pseudorandom_probability(card,'fading_charcuterie',card.ability.extra.basenum,card.ability.extra.basedenom,'fading_charcuterie') then
+                if not context.other_card:get_edition() then
+                    G.E_MANAGER:add_event(Event({
+                        trigger = "after",
+                        delay = 0.2,
+                        func = function()
+                            
+                            
+                            context.other_card:set_edition("e_charcuterie_bleached")
+                            
+                            return true
+                        end
+                    }))
+                   
+                    
+                end
+            end
+        end
     end
 }
